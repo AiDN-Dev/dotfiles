d1e6f5ce73bd13e65d5f57e19f09594628cef99b
AiDN-Dev <aidn.p1993@proton.me>
2025-11-17 13:11:44 +0000

    nord theme again xD, made a little more progress on my waybar, its not perfect at all but it kinda works which is nice

--- .config/waybar/power-menu.sh
+++ .config/waybar/power-menu.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-TMP_CONFIG_PATH="/tmp/waybar-power-menu.json"
+POWER_MENU_CONFIG_PATH="/tmp/waybar-power-menu.json"
 
-if pgrep -f "$TMP_CONFIG_PATH" >/dev/null; then
-  pkill -f "$TMP_CONFIG_PATH"
-else
-  cat >"$TMP_CONFIG_PATH" <<EOF
-{
-  "layer": "overlay",
-  "exclusive": false,
-  "width": 50,
-  "height": 50,
-  "anchor": "center",
-  "modules-center":["custom/lock","custom/restart","custom/shutdown"],
-  "custom/lock":{
-    "format": " Lock", 
-    "tooltip": false,
-    "on-click": "/usr/bin/hyprlock & pkill -f ${TMP_CONFIG_PATH}"
-  },
-  "custom/restart":{
-    "format": " restart",
-    "tooltip": false,
-    "on-click": "systemctl reboot"
-  },
-  "custom/shutdown": {
-    "format": " shutdown",
-    "tooltip": false,
-    "on-click": "systemctl poweroff"
+# function to kill any related waybar processe
+kill_waybar_powermenu() {
+  #kill the dimmer and power menu instances
+  pkill -f "$POWER_MENU_CONFIG_PATH"
+}
+
+# if any related power menu is running kill them
+if pgrep -f "$POWER_MENU_CONFIG_PATH" >/dev/null; then
+  kill_waybar_powermenu
+  exit 0
+fi
+#Create the config for the power menu
+cat >"$POWER_MENU_CONFIG_PATH" <<EOF
+  {
+    "layer": "overlay", 
+    "exclusive" : false, 
+    "width": 50, 
+    "height": 50, 
+    "anchor": "center",
+    "modules-center": ["custom/lock", "custom/restart", "custom/shutdown"],
+    "custom/lock": {
+      "format": " Lock", 
+      "tooltip": false, 
+      "on-click": "/usr/bin/hyprlock && kill_waybar_powermenu"
+    },
+    "custom/restart": {
+      "format": " Restart",
+      "tooltip": false,
+      "on-click": "systemctl reboot"
+    },
+    "custom/shutdown": {
+      "format": " Shutdown",
+      "tooltip": false,
+      "on-click": "systemctl poweroff"
+    }
   }
 }
 EOF
 
-  waybar -c "$TMP_CONFIG_PATH" -s ~/.config/waybar/power-menu.css &
-fi
+#Add the kill fucntion to the ecnvironment so it can be called from waybars on on-click
+export -f kill_waybar_powermenu
 
+waybar -c "$POWER_MENU_CONFIG_PATH" -s ~/.config/waybar/power-menu.css
